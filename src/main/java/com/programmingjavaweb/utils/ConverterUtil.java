5f5864c0b774326c8ef7f2f89be74792c185a8af
nguyenhien0209 <nguyenhien.py.1996@gmail.com>
2019-10-31 09:13:21 +0700

    updated

---
+++ src/main/java/com/programmingjavaweb/utils/ConverterUtil.java
@@ -0,0 +1,20 @@
+package com.programmingjavaweb.utils;
+
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+
+public class ConverterUtil {
+
+    public <T> T resultToModel (T t, ResultSet rs) {
+        Field[] fields = t.getClass().getDeclaredFields();
+        for(Field field : fields) {
+            try {
+                field.setAccessible(true);
+                field.set(t, rs.getObject(field.getName().toLowerCase()));
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return null;
+    }
+}
